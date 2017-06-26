3529aacfe7850c55e49fce93bc2eb2e778aa77ee
nanshenghuan <775415606@qq.com>
2017-06-27 00:54:51 +0800

    test

---
+++ Kerberos/src/BasicClass/Test.java
@@ -0,0 +1,24 @@
+package BasicClass;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ASserver="ASserver";
+		String TGSserver="TGSserver";
+		String APPLICATIONserver="APPLICATIONserver";
+		
+		
+		
+		
+		System.out.println(ASserver.hashCode());
+		System.out.println(TGSserver.hashCode());
+		System.out.println(APPLICATIONserver.hashCode());
+		
+		
+		
+
+	}
+}
